e7a3b33dfcc0457a4acfe4d2c7703ccd78a4df89
yuhui <536430462@qq.com>
2024-09-06 10:15:11 +0800

    后端--添加购物车

---
+++ eleme-server/src/main/java/com/eleme/service/ShoppingCartService.java
@@ -0,0 +1,12 @@
+package com.eleme.service;
+
+import com.eleme.dto.ShoppingCartDTO;
+
+public interface ShoppingCartService {
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+}
